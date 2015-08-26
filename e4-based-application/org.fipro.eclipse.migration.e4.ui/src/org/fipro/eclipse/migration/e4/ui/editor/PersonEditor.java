7a3e0774e5645d2b0e8f0cb6bb5b6679bbb69b03
Simon Scholz <simon.scholz@vogella.com>
2015-08-26 10:40:02 +0200

    Migrated the PersonEditor to Eclipse 4
    Change-Id: I615f539f8ce5f271ef7dba9f067e39216d874c6b
    Signed-off-by: Simon Scholz <simon.scholz@vogella.com>

--- e4-based-application/org.fipro.eclipse.migration.e4.ui/src/org/fipro/eclipse/migration/e4/ui/editor/PersonEditor.java
+++ e4-based-application/org.fipro.eclipse.migration.e4.ui/src/org/fipro/eclipse/migration/e4/ui/editor/PersonEditor.java
@@ -2,11 +2,17 @@ package org.fipro.eclipse.migration.e4.ui.editor;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 
 import org.eclipse.core.databinding.DataBindingContext;
 import org.eclipse.core.databinding.beans.BeanProperties;
 import org.eclipse.core.databinding.observable.value.IObservableValue;
-import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.e4.ui.di.Persist;
+import org.eclipse.e4.ui.model.application.ui.MDirtyable;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.jface.databinding.swt.WidgetProperties;
 import org.eclipse.jface.databinding.viewers.ViewersObservables;
 import org.eclipse.jface.layout.GridDataFactory;
@@ -19,65 +25,68 @@ import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.part.EditorPart;
 import org.fipro.eclipse.migration.e4.model.Person;
 import org.fipro.eclipse.migration.e4.model.Person.Gender;
 
+public class PersonEditor {
 
-public class PersonEditor extends EditorPart {
+	@Inject
+	private MDirtyable dirtyable;
 
 	public static final String ID = "org.fipro.eclipse.migration.ui.editor.personeditor";
 	
+	public static final String CONTRIBUTION_URI = "bundleclass://org.fipro.eclipse.migration.e4.ui/org.fipro.eclipse.migration.e4.ui.editor.PersonEditor";
+
+	public static final String PERSON_INPUT_DATA = "personInputData";
+
 	Person person;
 	Person activePerson;
-	
+
 	boolean dirty = false;
-	
-	@Override
-	public void init(IEditorSite site, IEditorInput input) throws PartInitException {
-		if (!(input instanceof PersonEditorInput)) {
-			throw new RuntimeException("Wrong input");
-		}
+
+	@Inject
+	public void init(MPart part) throws PartInitException {
+		Map<String, Object> transientData = part.getTransientData();
 		
-		this.person = ((PersonEditorInput) input).person;
+		// note that we are using transient data here, because the editor is not persisted anyway.
+		// In order to persist the editor between sessions using part.getPersistedState(); is necessary
+		if (!(transientData.get(PERSON_INPUT_DATA) instanceof Person)) {
+			throw new RuntimeException("You forgot to pass the actual person as transient data input");
+		}
+
+		this.person = (Person) part.getTransientData().get(PERSON_INPUT_DATA);
 		this.activePerson = new Person(this.person);
-	    setSite(site);
-	    setInput(input);
-	    setPartName(input.getName());
 	}
-	
-	@Override
+
+	@PostConstruct
 	public void createPartControl(Composite parent) {
 		parent.setLayout(new GridLayout(2, false));
-		
+
 		Label firstNameLabel = new Label(parent, SWT.NONE);
 		firstNameLabel.setText("Firstname:");
 		GridDataFactory.defaultsFor(firstNameLabel).applyTo(firstNameLabel);
-		
+
 		Text firstNameField = new Text(parent, SWT.BORDER);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(firstNameField);
-		
+
 		Label lastNameLabel = new Label(parent, SWT.NONE);
 		lastNameLabel.setText("Lastname:");
 		GridDataFactory.defaultsFor(lastNameLabel).applyTo(lastNameLabel);
-		
+
 		Text lastNameField = new Text(parent, SWT.BORDER);
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(lastNameField);
-		
+
 		Label marriedLabel = new Label(parent, SWT.NONE);
 		marriedLabel.setText("Married:");
 		GridDataFactory.defaultsFor(marriedLabel).applyTo(marriedLabel);
-		
+
 		Button marriedButton = new Button(parent, SWT.CHECK);
-		
+
 		Label genderLabel = new Label(parent, SWT.NONE);
 		genderLabel.setText("Gender:");
 		GridDataFactory.defaultsFor(marriedLabel).applyTo(genderLabel);
-		
+
 		ComboViewer genderCombo = new ComboViewer(parent);
 		genderCombo.setContentProvider(ArrayContentProvider.getInstance());
 		genderCombo.setLabelProvider(new LabelProvider() {
@@ -88,71 +97,40 @@ public class PersonEditor extends EditorPart {
 		});
 		genderCombo.setInput(Gender.values());
 		GridDataFactory.fillDefaults().grab(true, false).applyTo(genderCombo.getControl());
-		
+
 		// add bindings
 		DataBindingContext ctx = new DataBindingContext();
-		IObservableValue fnTarget = 
-				WidgetProperties.text(SWT.Modify).observe(firstNameField);
-		IObservableValue lnTarget = 
-				WidgetProperties.text(SWT.Modify).observe(lastNameField);
-		IObservableValue mTarget = 
-				WidgetProperties.selection().observe(marriedButton);
-		IObservableValue gTarget = 
-				ViewersObservables.observeSingleSelection(genderCombo);
-		
-		IObservableValue fnModel= BeanProperties.
-				value(Person.class,"firstName").observe(activePerson);
-		IObservableValue lnModel= BeanProperties.
-				value(Person.class,"lastName").observe(activePerson);
-		IObservableValue mModel= BeanProperties.
-				value(Person.class,"married").observe(activePerson);
-		IObservableValue gModel= BeanProperties.
-				value(Person.class,"gender").observe(activePerson);
-		
-		ctx.bindValue(fnTarget, fnModel); 
-		ctx.bindValue(lnTarget, lnModel); 
+		IObservableValue fnTarget = WidgetProperties.text(SWT.Modify).observe(firstNameField);
+		IObservableValue lnTarget = WidgetProperties.text(SWT.Modify).observe(lastNameField);
+		IObservableValue mTarget = WidgetProperties.selection().observe(marriedButton);
+		IObservableValue gTarget = ViewersObservables.observeSingleSelection(genderCombo);
+
+		IObservableValue fnModel = BeanProperties.value(Person.class, "firstName").observe(activePerson);
+		IObservableValue lnModel = BeanProperties.value(Person.class, "lastName").observe(activePerson);
+		IObservableValue mModel = BeanProperties.value(Person.class, "married").observe(activePerson);
+		IObservableValue gModel = BeanProperties.value(Person.class, "gender").observe(activePerson);
+
+		ctx.bindValue(fnTarget, fnModel);
+		ctx.bindValue(lnTarget, lnModel);
 		ctx.bindValue(mTarget, mModel);
 		ctx.bindValue(gTarget, gModel);
-		
+
 		this.activePerson.addPropertyChangeListener(new PropertyChangeListener() {
-			
+
 			@Override
 			public void propertyChange(PropertyChangeEvent evt) {
-				dirty = true;
-				firePropertyChange(IEditorPart.PROP_DIRTY);
+				dirtyable.setDirty(true);
 			}
 		});
 	}
-	
-	@Override
-	public void doSave(IProgressMonitor monitor) {
+
+	@Persist
+	public void save() {
 		this.person.setFirstName(this.activePerson.getFirstName());
 		this.person.setLastName(this.activePerson.getLastName());
 		this.person.setMarried(this.activePerson.isMarried());
 		this.person.setGender(this.activePerson.getGender());
-		
-		// TODO update table
-		
-		this.dirty = false;
-		firePropertyChange(IEditorPart.PROP_DIRTY);
-	}
 
-	@Override
-	public void doSaveAs() {
+		dirtyable.setDirty(false);
 	}
-
-	@Override
-	public boolean isDirty() {
-		return this.dirty;
-	}
-
-	@Override
-	public boolean isSaveAsAllowed() {
-		return false;
-	}
-
-	@Override
-	public void setFocus() {
-	}
-
 }
--- e4-based-application/org.fipro.eclipse.migration.e4.ui/src/org/fipro/eclipse/migration/e4/ui/view/overview/OverviewView.java
+++ e4-based-application/org.fipro.eclipse.migration.e4.ui/src/org/fipro/eclipse/migration/e4/ui/view/overview/OverviewView.java
@@ -9,12 +9,17 @@ import javax.inject.Inject;
 import org.eclipse.core.databinding.observable.IObservable;
 import org.eclipse.core.databinding.observable.list.WritableList;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
 import org.eclipse.jface.databinding.viewers.ObservableListContentProvider;
 import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.viewers.DoubleClickEvent;
 import org.eclipse.jface.viewers.IDoubleClickListener;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionChangedListener;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
@@ -24,99 +29,118 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IWorkbenchPage;
-import org.eclipse.ui.PartInitException;
 import org.fipro.eclipse.migration.e4.model.Person;
 import org.fipro.eclipse.migration.e4.service.PersonService;
 import org.fipro.eclipse.migration.e4.ui.editor.PersonEditor;
-import org.fipro.eclipse.migration.e4.ui.editor.PersonEditorInput;
 
 public class OverviewView {
 
 	TableViewer viewer;
-	
+
 	@Inject
 	private ESelectionService selectionService;
-	
+
 	@Inject
 	private PersonService personService;
+
+	@Inject
+	private EPartService partService;
 	
+	@Inject
+	private EModelService modelService;
+	
+	@Inject
+	private MApplication app;
+
 	@PostConstruct
 	public void createPartControl(Composite parent, final IWorkbenchPage workbenchPage) {
 		parent.setLayout(new GridLayout());
-		
+
 		IObservable list = new WritableList(personService.getPersons(10), Person.class);
 
-		viewer = new TableViewer(parent, SWT.MULTI|SWT.BORDER|SWT.H_SCROLL|SWT.V_SCROLL|SWT.FULL_SELECTION);
+		viewer = new TableViewer(parent, SWT.MULTI | SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.FULL_SELECTION);
 		ObservableListContentProvider cp = new ObservableListContentProvider();
 		viewer.setContentProvider(cp);
 		viewer.getTable().setHeaderVisible(true);
 		viewer.getTable().setLinesVisible(true);
 		GridDataFactory.fillDefaults().grab(true, true).applyTo(viewer.getControl());
-		
+
 		TableViewerColumn column = new TableViewerColumn(viewer, SWT.NONE);
 		column.getColumn().setText("Firstname");
 		column.getColumn().setWidth(100);
 		column.setLabelProvider(new FirstNameLabelProvider());
-//		column.setEditingSupport(new FirstNameEditingSupport(viewer));
-		
+		// column.setEditingSupport(new FirstNameEditingSupport(viewer));
+
 		column = new TableViewerColumn(viewer, SWT.NONE);
 		column.getColumn().setText("Lastname");
 		column.getColumn().setWidth(100);
 		column.setLabelProvider(new LastNameLabelProvider());
-//		column.setEditingSupport(new LastNameEditingSupport(viewer));
+		// column.setEditingSupport(new LastNameEditingSupport(viewer));
 
 		column = new TableViewerColumn(viewer, SWT.NONE);
 		column.getColumn().setText("Married");
 		column.getColumn().setWidth(60);
 		column.setLabelProvider(new MarriedLabelProvider());
-//		column.setEditingSupport(new MarriedEditingSupport(viewer));
-		
+		// column.setEditingSupport(new MarriedEditingSupport(viewer));
+
 		column = new TableViewerColumn(viewer, SWT.NONE);
 		column.getColumn().setText("Gender");
 		column.getColumn().setWidth(80);
 		column.setLabelProvider(new GenderLabelProvider());
-//		column.setEditingSupport(new GenderEditingSupport(viewer));
-		
+		// column.setEditingSupport(new GenderEditingSupport(viewer));
+
 		viewer.setInput(list);
-		
+
 		// set the viewer as selection provider
-//		getSite().setSelectionProvider(viewer);
+		// getSite().setSelectionProvider(viewer);
 		viewer.addSelectionChangedListener(new ISelectionChangedListener() {
-			
+
 			@Override
 			public void selectionChanged(SelectionChangedEvent event) {
-				IStructuredSelection selection = (IStructuredSelection)event.getSelection();
-				Person myInstance = (Person)selection.getFirstElement();
+				IStructuredSelection selection = (IStructuredSelection) event.getSelection();
+				Person myInstance = (Person) selection.getFirstElement();
 				selectionService.setSelection(myInstance);
 			}
 		});
-		
+
 		// hook double click for opening an editor
 		viewer.addDoubleClickListener(new IDoubleClickListener() {
-			
+
 			@Override
 			public void doubleClick(DoubleClickEvent event) {
-				ISelection selection = viewer.getSelection();
-				if (selection != null && selection instanceof IStructuredSelection) {
-					Object obj = ((IStructuredSelection) selection).getFirstElement();
-					// if we had a selection lets open the editor
-					if (obj != null) {
-						Person person = (Person) obj;
-						person.addPropertyChangeListener(new PropertyChangeListener() {
-							
-							@Override
-							public void propertyChange(PropertyChangeEvent evt) {
-								viewer.refresh();
-							}
-						});
+				IStructuredSelection selection = viewer.getStructuredSelection();
+				Object obj = selection.getFirstElement();
+				// if we had a selection lets open the editor
+				if (obj != null) {
+					Person person = (Person) obj;
+					person.addPropertyChangeListener(new PropertyChangeListener() {
 
-						PersonEditorInput input = new PersonEditorInput(person);
-				        try {
-				        	workbenchPage.openEditor(input, PersonEditor.ID);
-				        } catch (PartInitException e) {
-				        	throw new RuntimeException(e);
-				        }
-					}
+						@Override
+						public void propertyChange(PropertyChangeEvent evt) {
+							viewer.refresh();
+						}
+					});
+
+					MPart personEditor = modelService.createModelElement(MPart.class);
+					
+					personEditor.setContributionURI(PersonEditor.CONTRIBUTION_URI);
+					personEditor.setIconURI("platform:/plugin/org.fipro.eclipse.migration.e4.ui/icons/editor.gif");
+					personEditor.setElementId(PersonEditor.ID);
+					String label = person.getFirstName() + " " + person.getLastName();
+					personEditor.setLabel(label);
+					personEditor.setTooltip(label + " editor");
+					personEditor.setCloseable(true);
+					personEditor.getTags().add("Editor");
+
+					// set the person as transient data
+					personEditor.getTransientData().put(PersonEditor.PERSON_INPUT_DATA, person);
+
+					// find the editorss partstack, which is defined in the perspective snippet in the fragment.e4xmi
+					MPartStack stack = (MPartStack) modelService.find(
+							"org.eclipse.ui.editorss", app);
+					stack.getChildren().add(personEditor);
+					
+					partService.showPart(personEditor, PartState.ACTIVATE);
 				}
 			}
 		});
