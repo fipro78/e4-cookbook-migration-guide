ac358552f627a1de5d64fd2d64b366bec63e0da7
Dirk Fauth <dirk.fauth@googlemail.com>
2015-09-07 16:50:00 +0200

    Added save handler and configured window correctly in plain e4 app

---
+++ e4-based-application/org.fipro.eclipse.migration.e4.ui/src/org/fipro/eclipse/migration/e4/ui/handler/SaveHandler.java
@@ -0,0 +1,21 @@
+package org.fipro.eclipse.migration.e4.ui.handler;
+
+import org.eclipse.e4.core.di.annotations.CanExecute;
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+
+public class SaveHandler {
+	
+	@Execute
+	public void execute(EPartService partService) {
+		partService.saveAll(false);
+	}
+	
+	@CanExecute
+	public boolean canExecute(EPartService partService) {
+		if (partService != null) {
+			return !partService.getDirtyParts().isEmpty();
+		}
+		return false;
+	}
+}
